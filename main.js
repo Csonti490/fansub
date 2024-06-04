b35a8a89fe430433be020a7773470a282818ccf2
CsontiXD <113976041+Csonti490@users.noreply.github.com>
2024-06-04 21:38:22 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 /*Oldalcím és háttér*/
-window.onload = (event) => {
-    Keptolt();
-};
-var pictures = ['1.png','2.png','3.jpg','4.jpg','5.jpg','6.jpg'];
-var kephely = document.getElementById("httr");
 
-function Keptolt(){
-    var rand = Math.floor(Math.random() * pictures.length);
-    kephely.style.backgroundImage = "url(img/bgc/kep"+pictures[rand]+")";
+var pictures = ['1.png','2.png','3.jpg','4.jpg','5.jpg','6.jpg'];
+
+var kephely = document.getElementById("httr");
+if(kephely){
+    window.onload = (event) => {
+        Keptolt();
+    };
+    function Keptolt(){
+        var rand = Math.floor(Math.random() * pictures.length);
+        kephely.style.backgroundImage = "url(img/bgc/kep"+pictures[rand]+")";
+    }
 }
+
 
 function KepHiba(hibas){
     hibas.src = "img/sad1.jpg";
